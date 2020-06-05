7a0d8d5d3436f4320b11b7d125a27f756b187ad0
balachandranVishnu <visbaligithub@gmail.com>
2020-06-05 09:06:00 +0530

    Added the solution file and views

---
+++ Corp.EmployeeManagement.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Corp.Employee.Web.Models;
+
+namespace Corp.Employee.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ImportArea()
+        {
+            return View();
+        }
+
+        public IActionResult ViewArea()
+        {
+            return View();
+        }
+
+        public IActionResult SearchArea()
+        {
+            return View();
+        }
+
+        public IActionResult ResultsArea()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
